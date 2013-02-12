ffabdd9abecf6e631bc7a204baad07237a7f9bf4
Philip Chase <pbc@ufl.edu>
2013-02-12 17:27:32 -0500

    Move domain-joined test into join_domain exec
    Add unless test in join_domain exec.
    Set path in update_DNS exec.
    Added 204 as valid return value of update-dns

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@ class pbis (
   $ou = undef,
   $userDomainPrefix = undef,
   $assumeDefaultDomain = true,
-  $disable = undef,
+  $disable = undef
   ) {
 
   # PowerBroker Identity Services – Open Edition is not packaged for Red Hat, Fedora, or CentOS
@@ -25,48 +25,48 @@ class pbis (
     require    => Package['pbis-open'],
   }
   
+  # Construct the domainjoin-cli options string
+  if $ou {
+    $pbisOU = getOU($ou)
+    $optionOU = "--ou ${pbisOU}"
+  }
+  else {
+    $optionOU = ''
+  }
+  if $userDomainPrefix {
+    $optionPrefix = "--userDomainPrefix ${userDomainPrefix}"
+  }
+  else {
+    $optionPrefix = ''
+  }
+  if $assumeDefaultDomain == true {
+    $optionAssume = "--assumeDefaultDomain yes"
+  }
+  else {
+    $optionAssume = "--assumeDefaultDomain no"
+  }
+  if $disable {
+    $optionDisable = "--disable ${disable}"
+  }
+  else {
+    $optionDisable = ""
+  }
+    
+  $options = "${optionOU} ${optionPrefix} ${optionAssume} ${optionDisable}"
+    
   # Join the machine if it is not already on the domain.
-  if $adDomain != $domain {
-    # Construct the domainjoin-cli options string
-    if $ou {
-      $pbisOU = getOU($ou)
-      $optionOU = "--ou ${pbisOU}"
-    }
-    else {
-      $optionOU = ''
-    }
-    if $userDomainPrefix {
-      $optionPrefix = "--userDomainPrefix ${userDomainPrefix}"
-    }
-    else {
-      $optionPrefix = ''
-    }
-    if $assumeDefaultDomain == true {
-      $optionAssume = "--assumeDefaultDomain yes"
-    }
-    else {
-      $optionAssume = "--assumeDefaultDomain no"
-    }
-    if $disable {
-      $optionDisable = "--disable ${$disable}"
-    }
-    else {
-      $optionDisable = ""
-    }
-    
-    $options = "${optionOU} ${optionPrefix} ${optionAssume} ${optionDisable}"
-    
-    exec { 'join_domain':
-      path    => ['/usr/bin'],
-      command => "domainjoin-cli join ${options} ${adDomain} ${bindUsername} ${bindPassword}",
-      require => Service['lwsmd'],
-    }
-    
-    # Update DNS
-    exec { 'update_DNS':
-      path    => ['/opt/pbis/bin'],
-      command => "update-dns",
-      require => Exec['join_domain'],
-    }
+  exec { 'join_domain':
+    path    => ['/usr/bin', '/bin'],
+    command => "domainjoin-cli join ${options} ${adDomain} ${bindUsername} ${bindPassword}",
+    require => Service['lwsmd'],
+    unless => "/opt/pbis/bin/lsa ad-get-machine account 2> /dev/null | grep 'NetBIOS Domain Name'",
+  }
+
+  # Update DNS
+  exec { 'update_DNS':
+    path    => ['/opt/pbis/bin'],
+    command => "/opt/pbis/bin/update-dns",
+    require => Exec['join_domain'],
+    returns => [0, 204],
   }
 }
