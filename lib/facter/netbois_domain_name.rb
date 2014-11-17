48a9093a89184f1575cbb9593c493236c2087484
Ben Woods <bwoods@stchas.edu>
2014-11-17 15:12:57 -0600

    Adding NetBIOS Domain Name fact

---
+++ lib/facter/netbois_domain_name.rb
@@ -0,0 +1,19 @@
+# Fact: domain_dns_name
+#
+# Purpose:
+# Return the DNS name of the domain that the machine is joined to with PBIS
+#
+# Resolution:
+# Parse output of /opt/pbis/bin/lsa ad-get-machine account and report the value
+#
+# Caveats:
+# Linux only
+#
+# Author: Ben Woods <woodsbw@gmail.com>
+Facter.add(:netbois_domain_name) do
+  setcode do
+    line_output = `/opt/pbis/bin/lsa ad-get-machine account | grep 'NetBIOS Domain Name'`.split(":")
+    data_to_return = line_output[1].strip
+    data_to_return
+  end
+end
